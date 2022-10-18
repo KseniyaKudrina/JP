04f373e8d32c07cdba2244c2a3e5c1062456dc39
KseniyaKudrina <knkudrina@gmail.com>
2022-10-18 23:12:31 +0700

    FizzBuzz algo and Tests added

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,78 @@
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+public class FizzBuzzTest {
+    //1. Positive testing Happy path
+    // if(start <= end)
+    //return array;
+    // start < end
+
+    @Test
+    public void testStartLessThanEnd_HappyPath(){
+       //AAA
+       //Arrange
+        int start = 1;
+        int end = 20;
+        String[] expectedResult = {"1", "2", "Fizz", "4", "Buzz",
+        "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14",
+        "FizzBuzz", "16", "17", "Fizz", "19", "Buzz"};
+        // Act актуальный результат
+        String[] actualResult = new FizzBuzz().fizzBuzz(start, end);
+
+        //Assert
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+ // start == end
+    @Test
+    public void testStartEqualsEnd_HappyPath(){
+     //AAA
+     //Arrange
+     int start = 1;
+     int end = 1;
+     String[] expectedResult = {"1"};
+
+     // Act актуальный результат
+     String[] actualResult = new FizzBuzz().fizzBuzz(start, end);
+
+     //Assert
+     Assert.assertEquals(actualResult, expectedResult);
+ }
+// start < end
+    @Ignore
+    @Test
+    public void testStartLessThanEnd_StartEndAreNegative_HappyPath(){
+       //AAA
+       //Arrange
+        int start = -20;
+        int end = -1;
+        String[] expectedResult = { "Buzz", "..."}; // в обратную сторону
+        // Act актуальный результат
+        String[] actualResult = new FizzBuzz().fizzBuzz(start, end);
+
+        //Assert
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+    // 2. Negative testing
+    //      if (start > end)
+    // return new String[0]
+
+    @Test
+    public void testStartGreaterThanEnd_Negative(){
+        //AAA
+        //Arrange
+        int start = 20;
+        int end = 1;
+        String[] expectedResult = {};
+        // Act актуальный результат
+        String[] actualResult = new FizzBuzz().fizzBuzz(start, end);
+
+        //Assert
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+
+
+
+}
